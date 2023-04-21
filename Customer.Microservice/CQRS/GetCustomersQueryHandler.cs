228180f9cd33c4b7e3a2e586a7930386401b79e5
Billy <bimganda@gmail.com>
2023-04-21 18:52:57 -0400

    add query handler class to get list of customers

---
+++ Customer.Microservice/CQRS/GetCustomersQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Customer.Microservice.DTOs;
+using Customer.Microservice.Services;
+using MediatR;
+
+namespace Customer.Microservice.CQRS
+{
+    public class GetCustomersQueryHandler : IRequestHandler<GetCustomersQuery, List<GetCustomerDto>>
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public GetCustomersQueryHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<List<GetCustomerDto>> Handle(GetCustomersQuery request, CancellationToken cancellationToken)
+        {
+            var customers = await _customerRepository.GetAllAsync();
+            var result = customers.Select(c => new GetCustomerDto
+            {
+                Id = c.Id,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Email = c.Email,
+                PhoneNumber = c.PhoneNumber,
+                City = c.City,
+                State = c.State,
+                ZipCode = c.ZipCode,
+            }).ToList();
+
+            return result;
+        }
+    }
+}
