21a9590b886d087f572f2f7a990b32f184ffeb0e
Billy <bimganda@gmail.com>
2023-04-21 19:00:17 -0400

    add command class to add a customer

---
+++ Customer.Microservice/CQRS/AddCustomerCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Customer.Microservice.CQRS
+{
+    public class AddCustomerCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+    }
+}
