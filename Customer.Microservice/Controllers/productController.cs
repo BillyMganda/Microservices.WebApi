a3652676db1b4f587e00171f5b64b8a8abd9094d
Billy <bimganda@gmail.com>
2023-04-21 19:23:28 -0400

    add controller and a method to get all cutomers

---
+++ Customer.Microservice/Controllers/productController.cs
@@ -0,0 +1,27 @@
+﻿using Customer.Microservice.CQRS;
+using Customer.Microservice.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public productController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<GetCustomerDto>>> GetAll()
+        {
+            var query = new GetCustomersQuery();
+            var customers = await _mediator.Send(query);
+
+            return Ok(customers);
+        }
+    }
+}
