ee7aa96206a64ba87b80fb648a96f58401e6d24f
Billy <bimganda@gmail.com>
2023-04-21 18:14:58 -0400

    modify class to include new fields

--- Customer.Microservice/Models/CustomerEntity.cs
+++ Customer.Microservice/Models/CustomerEntity.cs
@@ -10,5 +10,8 @@
         public string City { get; set; } = string.Empty;
         public string State { get; set; } = string.Empty;
         public string ZipCode { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; }        
+        public DateTime LastModifiedDate { get; set; }
+        public bool IsActive { get; set; }
     }
 }
