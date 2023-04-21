5879838e05e7f2be8154a694e51626c0978e66e3
Billy <bimganda@gmail.com>
2023-04-21 18:47:24 -0400

    add query handler class to get customer by id

---
+++ Customer.Microservice/CQRS/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Customer.Microservice.DTOs;
+using Customer.Microservice.Services;
+using MediatR;
+
+namespace Customer.Microservice.CQRS
+{
+    public class GetCustomerByIdQueryHandler : IRequestHandler<GetCustomerByIdQuery, GetCustomerDto>
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public GetCustomerByIdQueryHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<GetCustomerDto> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var customer = await _customerRepository.GetByIdAsync(request.Id);
+            if (customer == null)
+            {
+                throw new Exception($"Customer with id {request.Id} not found.");
+            }
+
+            var result = new GetCustomerDto
+            {
+                Id = customer.Id,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Email = customer.Email,
+                PhoneNumber = customer.PhoneNumber,                
+                City = customer.City,
+                State = customer.State,
+                ZipCode = customer.ZipCode
+            };
+
+            return result;
+        }
+    }
+}
