0f1ab318edd6001f68991acfc523369a1579ac76
Billy <bimganda@gmail.com>
2023-04-24 07:32:43 -0400

    add command hander class to update a product

---
+++ Product.Microservice/CQRS/UpdateProductCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Product.Microservice.Services;
+
+namespace Product.Microservice.CQRS
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, string>
+    {
+        private readonly IProductRepository _repository;
+        public UpdateProductCommandHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<string> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var Product = await _repository.GetByIdAsync(request.Id);
+            if (Product == null)
+            {
+                throw new ArgumentException($"Product with Id {request.Id} not found.");
+            }
+
+            Product.Name = request.Name;
+            Product.Description = request.Description;
+            Product.Price = request.Price;
+            Product.LastModifiedDate = DateTime.Now;
+
+            await _repository.UpdateAsync(Product);
+            return "Product updated successfully.";
+        }
+    }
+}
