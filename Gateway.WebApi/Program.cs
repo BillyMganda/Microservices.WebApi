b4bb78b518c8fcb4b581bd3928f78584b5322565
Billy <bimganda@gmail.com>
2023-04-24 10:39:05 -0400

    add swashbackle package to project and update program file to use it

--- Gateway.WebApi/Program.cs
+++ Gateway.WebApi/Program.cs
@@ -2,9 +2,16 @@ using Ocelot.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.Run();
