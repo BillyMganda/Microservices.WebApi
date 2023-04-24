7f0861e7a0ffe18773d4a262d40454d15a1fff72
Billy <bimganda@gmail.com>
2023-04-24 06:39:36 -0400

    create product repository class that implements iproduct repository interface

---
+++ Product.Microservice/Services/ProductRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Microservice.Data;
+using Product.Microservice.Models;
+
+namespace Product.Microservice.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDbContext _context;
+        public ProductRepository(ProductDbContext productDbContext)
+        {
+            _context = productDbContext;
+        }
+
+        public async Task<ProductEntity> CreateAsync(ProductEntity entity)
+        {
+            await _context.Products.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<ProductEntity> DeleteAsync(ProductEntity entity)
+        {
+            var result = await _context.Products.FirstOrDefaultAsync(x => x.Id == entity.Id);
+            _context.Products.Remove(result);
+            await _context.SaveChangesAsync();
+            return result;
+        }
+
+        public async Task<IReadOnlyList<ProductEntity>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<ProductEntity> GetByIdAsync(Guid Id)
+        {
+            var result = await _context.Products.FindAsync(Id);
+            return result;
+        }
+
+        public async Task<ProductEntity> UpdateAsync(ProductEntity entity)
+        {
+            var existingEntity = await _context.Products.FindAsync(entity.Id);
+            if (existingEntity == null)
+            {
+                throw new Exception($"Product with id {entity.Id} not found.");
+            }
+
+            existingEntity.Name = entity.Name;
+            existingEntity.Description = entity.Description;
+            existingEntity.Price = entity.Price;            
+            existingEntity.LastModifiedDate = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            return existingEntity;
+        }
+    }
+}
