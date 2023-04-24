b8be3e45c6f5a51a6f9fb587a9ddb773509c4651
Billy <bimganda@gmail.com>
2023-04-24 07:14:11 -0400

    add query handler class to get customer by id

---
+++ Product.Microservice/CQRS/GetProductByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Product.Microservice.DTOs;
+using Product.Microservice.Services;
+
+namespace Product.Microservice.CQRS
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, GetProductDto>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<GetProductDto> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+            {
+                throw new Exception($"Product with id {request.Id} not found.");
+            }
+
+            var result = new GetProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                LastModifiedDate = product.LastModifiedDate,
+            };
+
+            return result;
+        }
+    }
+}
