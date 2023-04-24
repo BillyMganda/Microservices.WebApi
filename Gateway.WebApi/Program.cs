fb8124a61a68c6c6cc39d5e8657f1d36eb952744
Billy <bimganda@gmail.com>
2023-04-24 10:12:43 -0400

    update json files for the two projects, add ocelot.json file

--- Gateway.WebApi/Program.cs
+++ Gateway.WebApi/Program.cs
@@ -1,6 +1,10 @@
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
