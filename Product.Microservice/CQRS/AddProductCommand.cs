9d2dcfd12c464db9926ef84bc5da02452e0c5736
Billy <bimganda@gmail.com>
2023-04-24 07:17:31 -0400

    add command class to get add a product

---
+++ Product.Microservice/CQRS/AddProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Product.Microservice.CQRS
+{
+    public class AddProductCommand : IRequest<Guid>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }        
+    }
+}
