c53597ea444904b41144d78faa6c8c317ee49726
Billy <bimganda@gmail.com>
2023-04-24 06:11:43 -0400

    add project db context class

---
+++ Product.Microservice/Data/ProductDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Microservice.Models;
+
+namespace Product.Microservice.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+    }
+}
