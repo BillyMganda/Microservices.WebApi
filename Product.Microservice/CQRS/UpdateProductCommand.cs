e1a864771688894b65688cf83323107ede0a090f
Billy <bimganda@gmail.com>
2023-04-24 07:28:02 -0400

    add command class to update a product

---
+++ Product.Microservice/CQRS/UpdateProductCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Product.Microservice.CQRS
+{
+    public class UpdateProductCommand : IRequest<string>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Decimal Price { get; set; }
+    }
+}
