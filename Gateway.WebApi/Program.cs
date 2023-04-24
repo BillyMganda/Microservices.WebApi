27025d2c173ddb1c8f706c885382a94160ff9ab8
Billy <bimganda@gmail.com>
2023-04-24 10:48:50 -0400

    delete apigateway project

--- Gateway.WebApi/Program.cs
+++ Gateway.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,4 +15,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseOcelot();
 app.Run();
