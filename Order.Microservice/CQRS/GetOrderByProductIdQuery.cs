8db995cf14bef670e64def23d93a4929bc9ba678
Billy <bimganda@gmail.com>
2023-04-25 07:08:10 -0400

    add query class to get a list of orders by product id

---
+++ Order.Microservice/CQRS/GetOrderByProductIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Order.Microservice.DTOs;
+
+namespace Order.Microservice.CQRS
+{
+    public class GetOrderByProductIdQuery : IRequest<List<GetOrderDto>>
+    {
+        public Guid ProductId { get; set; }
+    }
+}
