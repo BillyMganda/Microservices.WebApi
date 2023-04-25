3b844be07f526baad93b29637c6fa496ad68c189
Billy <bimganda@gmail.com>
2023-04-25 03:30:32 -0400

    update repository class to implement all methods

--- Order.Microservice/Services/OrderRepository.cs
+++ Order.Microservice/Services/OrderRepository.cs
@@ -1,7 +1,55 @@
-﻿namespace Order.Microservice.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Microservice.Data;
+using Order.Microservice.Models;
+
+namespace Order.Microservice.Services
 {
     public class OrderRepository : IOrderRepository
     {
-        
+        private readonly OrderDbContext _context;
+        public OrderRepository(OrderDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public Task<OrderEntity> CreateAsync(List<OrderEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderEntity> DeleteAsync(OrderEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<OrderEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderEntity> GetByCustomerIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderEntity> GetByOrderDateAsync(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderEntity> GetByOrderIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderEntity> GetByProductIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderEntity> UpdateAsync(OrderEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
