60a1e42d2eca22405936ff5719efe0579d19b6c3
Billy <bimganda@gmail.com>
2023-04-25 07:13:11 -0400

    add query handler class to get a list of orders by product id

---
+++ Order.Microservice/CQRS/GetOrderByProductIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Order.Microservice.DTOs;
+using Order.Microservice.Services;
+
+namespace Order.Microservice.CQRS
+{
+    public class GetOrderByProductIdQueryHandler : IRequestHandler<GetOrderByProductIdQuery, List<GetOrderDto>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        public GetOrderByProductIdQueryHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<List<GetOrderDto>> Handle(GetOrderByProductIdQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetByProductIdAsync(request.ProductId);
+
+            var orderDtos = orders.Select(o => new GetOrderDto
+            {
+                Id = o.Id,
+                CustomerId = o.CustomerId,
+                ProductId = o.ProductIds,
+                OrderDate = o.OrderDate,
+                Price = o.Price,
+                Quantity = o.Quantity
+            }).ToList();
+
+            return orderDtos;
+        }
+    }
+}
