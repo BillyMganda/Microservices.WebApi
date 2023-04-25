1efdedbf16e9727ae4276e2d7d6c6f3809e39a6e
Billy <bimganda@gmail.com>
2023-04-25 07:23:17 -0400

    add query handler class to get a list of orders by date

---
+++ Order.Microservice/CQRS/GetOrderByDateQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Order.Microservice.DTOs;
+using Order.Microservice.Services;
+
+namespace Order.Microservice.CQRS
+{
+    public class GetOrderByDateQueryHandler : IRequestHandler<GetOrderByDateQuery, List<GetOrderDto>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        public GetOrderByDateQueryHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<List<GetOrderDto>> Handle(GetOrderByDateQuery request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetByOrderDateAsync(request.Date);
+            if (order == null)
+            {
+                throw new Exception($"Order with date {request.Date} not found.");
+            }
+
+            var orderDtos = order.Select(o => new GetOrderDto
+            {
+                Id = o.Id,
+                CustomerId = o.CustomerId,
+                ProductId = o.ProductIds,
+                OrderDate = o.OrderDate,
+                Price = o.Price,
+                Quantity = o.Quantity
+            }).ToList();
+
+            return orderDtos;
+        }
+    }
+}
