4bbed1056f9b75302554997b1396574daf4afac9
Billy <bimganda@gmail.com>
2023-04-25 07:44:07 -0400

    add query handler class to delete an order by order id

---
+++ Order.Microservice/CQRS/DeleteOrderCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Order.Microservice.Services;
+
+namespace Order.Microservice.CQRS
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetByOrderIdAsync(request.OrderId);
+            if (order == null)
+            {
+                throw new ArgumentException($"Crder with Id {request.OrderId} not found.");
+            }
+            await _orderRepository.DeleteAsync(order);
+        }
+    }
+}
+ 
