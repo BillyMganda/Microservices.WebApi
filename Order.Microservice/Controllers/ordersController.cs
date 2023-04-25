8b3da894f863d3c982e142443a287aca75d38a89
Billy <bimganda@gmail.com>
2023-04-25 10:09:52 -0400

    add method to get all orders

--- Order.Microservice/Controllers/ordersController.cs
+++ Order.Microservice/Controllers/ordersController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Order.Microservice.CQRS;
+using Order.Microservice.DTOs;
 
 namespace Order.Microservice.Controllers
 {
@@ -7,5 +9,19 @@ namespace Order.Microservice.Controllers
     [ApiController]
     public class ordersController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        public ordersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<GetOrderDto>>> GetAll()
+        {
+            var query = new GetOrdersQuery();
+            var customers = await _mediator.Send(query);
+
+            return Ok(customers);
+        }
     }
 }
