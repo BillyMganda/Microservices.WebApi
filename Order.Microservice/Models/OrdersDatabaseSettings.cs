becab80eb8ee100f3d1c97e1ea1f6e019c691ce9
Billy <bimganda@gmail.com>
2023-04-25 10:44:33 -0400

    add class to mirror details in connection string in appsettings.json file

---
+++ Order.Microservice/Models/OrdersDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Order.Microservice.Models
+{
+    public class OrdersDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string OrdersCollectionName { get; set; } = null!;
+    }
+}
