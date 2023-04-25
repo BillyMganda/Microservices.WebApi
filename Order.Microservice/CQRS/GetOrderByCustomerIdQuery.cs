9f8cdcf51602043d468663c2d9b769e84509e5bd
Billy <bimganda@gmail.com>
2023-04-25 04:32:25 -0400

    add query class to get an order by customer id

---
+++ Order.Microservice/CQRS/GetOrderByCustomerIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Order.Microservice.DTOs;
+
+namespace Order.Microservice.CQRS
+{
+    public class GetOrderByCustomerIdQuery : IRequest<GetOrderDto>
+    {
+        public Guid CustomerId { get; set; }
+    }
+}
