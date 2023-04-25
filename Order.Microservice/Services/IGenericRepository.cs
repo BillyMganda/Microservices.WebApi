36e6fdca6b3a3654d5edae0205d20e0d255e72a0
Billy <bimganda@gmail.com>
2023-04-25 02:10:42 -0400

    add generic interface class with crud operations

---
+++ Order.Microservice/Services/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Order.Microservice.Services
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByOrderIdAsync(Guid Id);
+        Task<T> GetByCustomerIdAsync(Guid Id);
+        Task<T> GetByProductIdAsync(Guid Id);
+        Task<T> GetByOrderDateAsync(DateTime date);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> CreateAsync(List<T> entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+}
