d3bfc4eaa1e75c753c015a70ff80e62f43e40dfb
Billy <bimganda@gmail.com>
2023-04-25 04:37:56 -0400

    update interface to return a list of orders by a given product id

--- Order.Microservice/Services/IGenericRepository.cs
+++ Order.Microservice/Services/IGenericRepository.cs
@@ -4,7 +4,7 @@
     {
         Task<T> GetByOrderIdAsync(Guid Id);
         Task<T> GetByCustomerIdAsync(Guid Id);
-        Task<T> GetByProductIdAsync(Guid Id);
+        Task<List<T>> GetByProductIdAsync(Guid Id);
         Task<T> GetByOrderDateAsync(DateTime date);
         Task<IReadOnlyList<T>> GetAllAsync();
         Task<T> CreateAsync(T entity);        
