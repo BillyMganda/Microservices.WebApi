904706b796e21bde0a6dfa0211040c10f4704b73
Billy <bimganda@gmail.com>
2023-04-25 07:16:59 -0400

    add query class to get a list of orders by date

---
+++ Order.Microservice/CQRS/GetOrderByDateQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Order.Microservice.DTOs;
+
+namespace Order.Microservice.CQRS
+{
+    public class GetOrderByDateQuery : IRequest<List<GetOrderDto>>
+    {
+        public DateTime Date { get; set; }
+    }
+}
