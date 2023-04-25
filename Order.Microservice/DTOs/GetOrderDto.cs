a5aef2079ee85d38f6eb85f47440aee91fdb5b24
Billy <bimganda@gmail.com>
2023-04-25 01:52:07 -0400

    add dto class to get an order

---
+++ Order.Microservice/DTOs/GetOrderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Order.Microservice.DTOs
+{
+    public class GetOrderDto
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid ProductId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
