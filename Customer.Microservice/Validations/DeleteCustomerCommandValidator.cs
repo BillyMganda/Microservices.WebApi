cd2b9d782d162200c0c3f3a72d0f857a07745613
Billy <bimganda@gmail.com>
2023-04-26 07:02:24 -0400

    add validation class to deleting a customer command

---
+++ Customer.Microservice/Validations/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using Customer.Microservice.CQRS;
+using FluentValidation;
+
+namespace Customer.Microservice.Validations
+{
+    public class DeleteCustomerCommandValidator : AbstractValidator<DeleteCustomerCommand>
+    {
+        public DeleteCustomerCommandValidator()
+        {
+            RuleFor(x => x.Id)
+            .Must(id => Guid.TryParse(id.ToString(), out var _))
+            .WithMessage("Invalid GUID format.");
+        }
+    }
+}
