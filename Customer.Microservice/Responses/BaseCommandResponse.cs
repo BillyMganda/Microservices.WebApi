b2aab0e7a788ffe5c0f09b2d78f6c4d5b4e4c0de
Billy <bimganda@gmail.com>
2023-04-26 12:48:50 -0400

    add base command response class

---
+++ Customer.Microservice/Responses/BaseCommandResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Customer.Microservice.Responses
+{
+    public class BaseCommandResponse
+    {
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
