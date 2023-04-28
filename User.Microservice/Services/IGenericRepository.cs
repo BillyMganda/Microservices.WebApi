b361186eceb8a0e95dcd29cb0017eebd88eb5e8b
Billy <bimganda@gmail.com>
2023-04-28 08:29:37 -0400

    update interface class to include other methods

--- User.Microservice/Services/IGenericRepository.cs
+++ User.Microservice/Services/IGenericRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace User.Microservice.Services
+﻿using User.Microservice.DTOs;
+
+namespace User.Microservice.Services
 {
     public interface IGenericRepository<T> where T : class
     {
@@ -6,10 +8,12 @@
         Task<T> GetByEmailAsync(Guid Id);
         Task<T> CreateRefreshToken();
         void CreatePasswordHash(string password, out byte[] PasswordHash, out byte[] PasswordSalt);
-        bool VerifyPasswordHash(string password, byte[] PasswordHash, byte[] PasswordSalt);
+        bool VerifyPasswordHash(string password, byte[] PasswordHash, byte[] PasswordSalt);        
         string CreateJWTToken(LoginDto dto);
+        string ForgotPaswordToken();
         Task<T> CreateAsync(T entity);
         Task<T> ChangePasswordAsync(T entity);
         Task<T> DeactivateAsync(T entity);
+        void SendRegistrationEmail(EmailDto dto, string email, string fname);
     }
 }
