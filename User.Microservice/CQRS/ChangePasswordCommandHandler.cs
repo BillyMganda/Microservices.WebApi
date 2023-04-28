864c947129e2da97be3b16d40c7939a723cde169
Billy <bimganda@gmail.com>
2023-04-28 12:23:38 -0400

    add CQRS command handler to change a user password

---
+++ User.Microservice/CQRS/ChangePasswordCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using User.Microservice.DTOs;
+using User.Microservice.Services;
+
+namespace User.Microservice.CQRS
+{
+    public class ChangePasswordCommandHandler : IRequestHandler<ChangePasswordCommand, GetUserDto>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ChangePasswordCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<GetUserDto> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.ChangeUserPasswordAsync(request.changePassword, request.PasswordHash, request.PasswordSalt);
+            return user;
+        }
+    }
+}
