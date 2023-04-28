b15df4477d1e22d5b0f264608a6bc1a2ed98c29c
Billy <bimganda@gmail.com>
2023-04-28 03:43:56 -0400

    remove unnecessary clatter

--- Customer.Microservice/CQRS/AddCustomerCommandHandler.cs
+++ Customer.Microservice/CQRS/AddCustomerCommandHandler.cs
@@ -42,3 +42,4 @@ namespace Customer.Microservice.CQRS
         }
     }
 }
+ 
--- Customer.Microservice/Responses/BaseCommandResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Customer.Microservice.Responses
-{
-    public class BaseCommandResponse
-    {
-        public bool Success { get; set; } = true;
-        public string Message { get; set; } = string.Empty;
-        public List<string> Errors { get; set; } = new List<string>();
-    }
-}
