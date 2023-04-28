e26d702321790ae92ef579e1de7d8b895c78c9ae
Billy <bimganda@gmail.com>
2023-04-28 08:25:45 -0400

    add generic class with methods

---
+++ User.Microservice/Services/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace User.Microservice.Services
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid Id);
+        Task<T> GetByEmailAsync(Guid Id);
+        Task<T> CreateRefreshToken();
+        void CreatePasswordHash(string password, out byte[] PasswordHash, out byte[] PasswordSalt);
+        bool VerifyPasswordHash(string password, byte[] PasswordHash, byte[] PasswordSalt);
+        string CreateJWTToken(LoginDto dto);
+        Task<T> CreateAsync(T entity);
+        Task<T> ChangePasswordAsync(T entity);
+        Task<T> DeactivateAsync(T entity);
+    }
+}
