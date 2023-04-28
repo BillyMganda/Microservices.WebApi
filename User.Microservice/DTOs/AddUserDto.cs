703757e14e854fdad58700334ee8af8768b62e6e
Billy <bimganda@gmail.com>
2023-04-28 08:16:00 -0400

    add dto class for adding a new user

---
+++ User.Microservice/DTOs/AddUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace User.Microservice.DTOs
+{
+    public class AddUserDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
