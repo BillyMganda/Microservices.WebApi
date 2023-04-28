ec5bc3e125695c158b8815c146ce7c255ee3dd81
Billy <bimganda@gmail.com>
2023-04-28 13:15:01 -0400

    add users controller with method to register a user

---
+++ User.Microservice/Controllers/usersController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using User.Microservice.CQRS;
+
+namespace User.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public usersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody] CreateUserCommand command)
+        {
+            var userDto = await _mediator.Send(command);
+            return Ok(userDto);
+        }
+    }
+}
