7bb05576b8f434f7fa230d63eb43f3c6fc94b8ed
Billy <bimganda@gmail.com>
2023-04-28 13:10:10 -0400

    add CQRS command handler for forgetting user password

--- User.Microservice/CQRS/ForgotPasswordCommand.cs
+++ User.Microservice/CQRS/ForgotPasswordCommand.cs
@@ -2,7 +2,7 @@
 
 namespace User.Microservice.CQRS
 {
-    public class ForgotPasswordCommand : IRequest<string>
+    public class ForgotPasswordCommand : IRequest
     {
         public string Email { get; set; } = string.Empty;
     }
---
+++ User.Microservice/CQRS/ForgotPasswordCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using User.Microservice.Services;
+
+namespace User.Microservice.CQRS
+{
+    public class ForgotPasswordCommandHandler : IRequestHandler<ForgotPasswordCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        public ForgotPasswordCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+        {
+            _userRepository.ForgotPasswordControllerMethod(request.Email);    
+            return Task.CompletedTask;
+        }
+    }
+}
