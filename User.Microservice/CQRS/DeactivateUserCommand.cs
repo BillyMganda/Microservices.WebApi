f44897c8e6994cf0dee8733e0923c702f5d83761
Billy <bimganda@gmail.com>
2023-04-28 12:13:01 -0400

    add CQRS command to deactivate a user

---
+++ User.Microservice/CQRS/DeactivateUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using User.Microservice.DTOs;
+
+namespace User.Microservice.CQRS
+{
+    public class DeactivateUserCommand : IRequest<GetUserDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
