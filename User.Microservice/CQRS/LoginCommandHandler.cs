05bc2f09e6fc5ef2b887c076834ccfed5755936e
Billy <bimganda@gmail.com>
2023-04-28 12:43:37 -0400

    add CQRS command handler for user login

---
+++ User.Microservice/CQRS/LoginCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using User.Microservice.Services;
+
+namespace User.Microservice.CQRS
+{
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, string>
+    {
+        private readonly IUserRepository _userRepository;
+        public LoginCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var Jwt = _userRepository.Login(request.LoginDto);
+            return Jwt;
+        }
+    }
+}
