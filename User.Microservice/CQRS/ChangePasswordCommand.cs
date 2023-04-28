eb1a0a24c72149d0f404b96d504ea4a59619fe4e
Billy <bimganda@gmail.com>
2023-04-28 12:20:29 -0400

    add CQRS command to change a user password

---
+++ User.Microservice/CQRS/ChangePasswordCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using User.Microservice.DTOs;
+
+namespace User.Microservice.CQRS
+{
+    public class ChangePasswordCommand : IRequest<GetUserDto>
+    {
+        public ChangePasswordDto changePassword { get; set; }
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+    }
+}
