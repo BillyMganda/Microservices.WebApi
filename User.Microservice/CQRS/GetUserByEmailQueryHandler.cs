1676bde9b9f146a93eab4f256451fa96d1709d7f
Billy <bimganda@gmail.com>
2023-04-28 11:55:46 -0400

    add CQRS query handler to get user by email

---
+++ User.Microservice/CQRS/GetUserByEmailQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using User.Microservice.DTOs;
+using User.Microservice.Exceptions;
+using User.Microservice.Services;
+
+namespace User.Microservice.CQRS
+{
+    public class GetUserByEmailQueryHandler : IRequestHandler<GetUserByEmailQuery, GetUserDto>
+    {
+        private readonly IUserRepository _userRepository;
+        public GetUserByEmailQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<GetUserDto> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByEmailAsync(request.Email);
+            if (user == null)
+            {
+                throw new NotFoundException(nameof(User), request.Email);
+            }
+            return user;
+        }
+    }
+}
