65f61c5bec16cb9f317064094ab9c3a2bdc7a291
Billy <bimganda@gmail.com>
2023-04-28 08:57:02 -0400

    add methods to repository class

--- User.Microservice/Services/IUserRepository.cs
+++ User.Microservice/Services/IUserRepository.cs
@@ -1,8 +1,20 @@
-﻿using User.Microservice.Models;
+﻿using User.Microservice.DTOs;
 
 namespace User.Microservice.Services
 {
     public interface IUserRepository
     {
+        Task<GetUserDto> GetUserByIdAsync(Guid Id);
+        Task<GetUserDto> GetUserByEmailAsync(Guid Id);
+        Task<GetUserDto> CreateRefreshToken();
+        void CreatePasswordHash(string password, out byte[] PasswordHash, out byte[] PasswordSalt);
+        bool VerifyPasswordHash(string password, byte[] PasswordHash, byte[] PasswordSalt);
+        string CreateJWTToken(LoginDto dto);
+        string ForgotPaswordToken();
+        Task<GetUserDto> CreateUserAsync(AddUserDto entity);
+        Task<GetUserDto> ChangeUserPasswordAsync(ChangePasswordDto entity);
+        Task<GetUserDto> DeactivateUserAsync(Guid Id);
+        void SendRegistrationEmail(EmailDto dto);
+        void SendForgotPasswordEmail(EmailDto dto);
     }
 }
