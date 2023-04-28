1425728302a331b50ac2b9086d9100fe75ad6cef
Billy <bimganda@gmail.com>
2023-04-28 11:24:02 -0400

    implement method to create a forgot password token

--- User.Microservice/Services/UserRepository.cs
+++ User.Microservice/Services/UserRepository.cs
@@ -133,7 +133,9 @@ namespace User.Microservice.Services
 
         public string ForgotPaswordToken()
         {
-            throw new NotImplementedException();
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
         public Task<GetUserDto> GetUserByEmailAsync(Guid Id)
