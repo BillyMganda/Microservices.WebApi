56e12deb8fd62c582a5530e303f293f1822cd256
Billy <bimganda@gmail.com>
2023-04-28 07:20:31 -0400

    add user model class

---
+++ User.Microservice/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace User.Microservice.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string ForgotPasswordToken { get; set; } = string.Empty;
+        public bool IsTermsAgreed { get; set; }
+    }
+}
