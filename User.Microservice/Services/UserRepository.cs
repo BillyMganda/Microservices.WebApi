ccb3f5388229c2bb1c4770dbce279b9da6bf3cf7
Billy <bimganda@gmail.com>
2023-04-28 08:58:09 -0400

    add implementation of all methods throwing NotImplementedException

--- User.Microservice/Services/UserRepository.cs
+++ User.Microservice/Services/UserRepository.cs
@@ -11,6 +11,65 @@ namespace User.Microservice.Services
         {
             _dbContext = dbContext;
         }
-               
+
+        public Task<GetUserDto> ChangeUserPasswordAsync(ChangePasswordDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateJWTToken(LoginDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatePasswordHash(string password, out byte[] PasswordHash, out byte[] PasswordSalt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUserDto> CreateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUserDto> CreateUserAsync(AddUserDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUserDto> DeactivateUserAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ForgotPaswordToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUserDto> GetUserByEmailAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUserDto> GetUserByIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendForgotPasswordEmail(EmailDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendRegistrationEmail(EmailDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] PasswordHash, byte[] PasswordSalt)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
