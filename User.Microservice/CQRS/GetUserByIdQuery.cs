d3d3bae1fcb6bff8057cc956ed71514e5525204d
Billy <bimganda@gmail.com>
2023-04-28 11:43:38 -0400

    add CQRS query to get user by id

---
+++ User.Microservice/CQRS/GetUserByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using User.Microservice.DTOs;
+
+namespace User.Microservice.CQRS
+{
+    public class GetUserByIdQuery : IRequest<GetUserDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
