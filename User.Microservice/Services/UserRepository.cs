0bc705ebf69c596bffc8da317f2fa1b96f21506c
Billy <bimganda@gmail.com>
2023-04-28 08:49:47 -0400

    remove all methods awaiting editing of generic repository interface

--- User.Microservice/Services/UserRepository.cs
+++ User.Microservice/Services/UserRepository.cs
@@ -11,65 +11,6 @@ namespace User.Microservice.Services
         {
             _dbContext = dbContext;
         }
-
-        public Task<UserModel> ChangeUserPasswordAsync(UserModel entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string CreateJWTToken(LoginDto dto)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CreatePasswordHash(string password, out byte[] PasswordHash, out byte[] PasswordSalt)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserModel> CreateRefreshToken()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserModel> CreateUserAsync(UserModel entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserModel> DeactivateUserAsync(UserModel entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string ForgotPaswordToken()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserModel> GetUserByEmailAsync(Guid Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserModel> GetUserByIdAsync(Guid Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SendForgotPasswordEmail(EmailDto dto)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SendRegistrationEmail(EmailDto dto)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool VerifyPasswordHash(string password, byte[] PasswordHash, byte[] PasswordSalt)
-        {
-            throw new NotImplementedException();
-        }
+               
     }
 }
