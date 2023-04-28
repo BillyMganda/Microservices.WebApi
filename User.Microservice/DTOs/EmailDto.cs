6877e14b485e7d686f5b13ba660b5b1e66836967
Billy <bimganda@gmail.com>
2023-04-28 08:33:59 -0400

    add dto class for emails, add/edit methods in interface class

---
+++ User.Microservice/DTOs/EmailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace User.Microservice.DTOs
+{
+    public class EmailDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- User.Microservice/Services/IGenericRepository.cs
+++ User.Microservice/Services/IGenericRepository.cs
@@ -4,16 +4,17 @@ namespace User.Microservice.Services
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> GetByIdAsync(Guid Id);
-        Task<T> GetByEmailAsync(Guid Id);
+        Task<T> GetUserByIdAsync(Guid Id);
+        Task<T> GetUserByEmailAsync(Guid Id);
         Task<T> CreateRefreshToken();
         void CreatePasswordHash(string password, out byte[] PasswordHash, out byte[] PasswordSalt);
         bool VerifyPasswordHash(string password, byte[] PasswordHash, byte[] PasswordSalt);        
         string CreateJWTToken(LoginDto dto);
         string ForgotPaswordToken();
-        Task<T> CreateAsync(T entity);
-        Task<T> ChangePasswordAsync(T entity);
-        Task<T> DeactivateAsync(T entity);
-        void SendRegistrationEmail(EmailDto dto, string email, string fname);
+        Task<T> CreateUserAsync(T entity);
+        Task<T> ChangeUserPasswordAsync(T entity);
+        Task<T> DeactivateUserAsync(T entity);
+        void SendRegistrationEmail(EmailDto dto);
+        void SendForgotPasswordEmail(EmailDto dto);
     }
 }
