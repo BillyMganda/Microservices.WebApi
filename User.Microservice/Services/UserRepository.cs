fde44c7e1cead26ff59e8ed8fd6b3d97419c0739
Billy <bimganda@gmail.com>
2023-04-29 07:48:19 -0400

    rearrange order of methods

--- User.Microservice/Services/UserRepository.cs
+++ User.Microservice/Services/UserRepository.cs
@@ -149,12 +149,7 @@ namespace User.Microservice.Services
             return GetDto;
         }
 
-        public string ForgotPaswordToken()
-        {
-            Random random = new Random();
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
-        }
+        
 
         public async Task<GetUserDto> GetUserByEmailAsync(string Email)
         {
@@ -244,7 +239,13 @@ namespace User.Microservice.Services
                 return ComputedHash.SequenceEqual(PasswordHash);
             }
         }
-
+                
+        public string ForgotPaswordToken()
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
         public async Task<GetUserDto> UpdateForgotPasswordTokenInDb(string Email, string Token)
         {
             var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Email == Email);
