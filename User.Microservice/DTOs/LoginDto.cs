94114e329eb73c97005d411b9fca7e3190e95894
Billy <bimganda@gmail.com>
2023-04-29 07:34:59 -0400

    add data annotations to dto

--- User.Microservice/DTOs/LoginDto.cs
+++ User.Microservice/DTOs/LoginDto.cs
@@ -1,8 +1,12 @@
-﻿namespace User.Microservice.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.Microservice.DTOs
 {
     public class LoginDto
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required, MinLength(6)]
         public string Password { get; set; } = string.Empty;
     }
 }
