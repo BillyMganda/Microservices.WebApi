687ac8ad7afa9e7136851718c0a5364a4e7a7258
Billy <bimganda@gmail.com>
2023-05-01 07:37:58 -0400

    remove all try - catch blocks from controller

--- User.Microservice/Controllers/usersController.cs
+++ User.Microservice/Controllers/usersController.cs
@@ -17,57 +17,29 @@ namespace User.Microservice.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> RegisterUser([FromBody] CreateUserCommand command)
         {
-            try
-            {
-                var userDto = await _mediator.Send(command);
-                return Ok(userDto);
-            }
-            catch (Exception)
-            {
-                throw;
-            }            
+            var userDto = await _mediator.Send(command);
+            return Ok(userDto);
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> UserLogin([FromBody] LoginCommand command)
         {
-            try
-            {
-                var Jwt = await _mediator.Send(command);
-                return Ok(Jwt);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            var Jwt = await _mediator.Send(command);
+            return Ok(Jwt);
         }
 
         [HttpPost("forgot-password")]
         public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordCommand command)
         {
-            try
-            {
-                var user = await _mediator.Send(command);
-                return Ok("Request successful, check your email for password reset token");
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            var user = await _mediator.Send(command);
+            return Ok("Request successful, check your email for password reset token");
         }
 
         [HttpPost("reset-password")]
         public async Task<IActionResult> ResetPassword([FromBody] ChangePasswordCommand command)
         {
-            try
-            {
-                var user = await _mediator.Send(command);
-                return Ok("Operation successful");
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            var user = await _mediator.Send(command);
+            return Ok("Operation successful");
         }
     }
 }
