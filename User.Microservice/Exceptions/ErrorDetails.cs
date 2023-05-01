6cc6f5ec05a596fcbbf840ac7c2c6b306e3a4759
Billy <bimganda@gmail.com>
2023-05-01 06:53:19 -0400

    add an error details class

---
+++ User.Microservice/Exceptions/ErrorDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace User.Microservice.Exceptions
+{
+    public class ErrorDetails
+    {
+        public string ErrorType { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
