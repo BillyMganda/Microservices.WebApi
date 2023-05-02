9c8ec715d335dd2aefe13aa0c7b3ba5e5e1f8222
Billy <bimganda@gmail.com>
2023-05-02 04:15:20 -0400

    update login command to use direct properties in place of a dto

--- User.Microservice/CQRS/LoginCommand.cs
+++ User.Microservice/CQRS/LoginCommand.cs
@@ -1,10 +1,14 @@
 ﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
 using User.Microservice.DTOs;
 
 namespace User.Microservice.CQRS
 {
     public class LoginCommand : IRequest<string>
     {
-        public LoginDto LoginDto { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
     }
 }
