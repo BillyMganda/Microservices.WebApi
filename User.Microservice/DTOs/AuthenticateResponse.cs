a879afd4e50d04b9589f5227ce1862678e092f07
Billy <bimganda@gmail.com>
2023-05-03 06:41:51 -0400

    add authentication response class

---
+++ User.Microservice/DTOs/AuthenticateResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using User.Microservice.Models;
+
+namespace User.Microservice.DTOs
+{
+    public class AuthenticateResponse
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JwtToken { get; set; }
+        [JsonIgnore] // refresh token is returned in http only cookie
+        public string RefreshToken { get; set; }
+
+        public AuthenticateResponse(UserModel user, string jwtToken, string refreshToken)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            JwtToken = jwtToken;
+            RefreshToken = refreshToken;
+        }
+    }
+}
+    
