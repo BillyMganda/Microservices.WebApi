4ec72ce2655fb1496eaf58a212252ae9af06cca0
Billy <bimganda@gmail.com>
2023-05-04 06:58:06 -0400

    add jwt utils interface

---
+++ User.Microservice/Services/IJwtUtils.cs
@@ -0,0 +1,11 @@
+﻿using User.Microservice.Models;
+
+namespace User.Microservice.Services
+{
+    public interface IJwtUtils
+    {
+        public string GenerateJwtToken(UserModel user);
+        public int? ValidateJwtToken(string token);
+        public RefreshToken GenerateRefreshToken(string ipAddress);
+    }
+}
