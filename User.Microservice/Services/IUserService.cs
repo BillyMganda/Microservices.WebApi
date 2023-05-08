003f94ef37e013d2e46c676c45806a59637266b0
Billy <bimganda@gmail.com>
2023-05-08 02:07:28 -0400

    create user interface class with all the necessary methods

---
+++ User.Microservice/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using User.Microservice.DTOs;
+using User.Microservice.Entities;
+
+namespace User.Microservice.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model, string ipAddress);
+        AuthenticateResponse RefreshToken(string token, string ipAddress);
+        void RevokeToken(string token, string ipAddress);
+        IEnumerable<UserModel> GetAll();
+        UserModel GetById(Guid id);
+    }
+}
