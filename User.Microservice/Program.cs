52a95c007aa61b8a44bdd3bdcc2fcdfe9573a6f3
Billy <bimganda@gmail.com>
2023-05-08 05:08:59 -0400

    add migration to db and work on program.cs to enable system to run on startup and counter {"message":"Value cannot be null. (Parameter \u0027input\u0027)"} exception

--- User.Microservice/Program.cs
+++ User.Microservice/Program.cs
@@ -1,10 +1,6 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
-using System.Reflection;
-using System.Text;
 using System.Text.Json.Serialization;
 using User.Microservice.Authorization;
 using User.Microservice.Helpers;
@@ -12,12 +8,12 @@ using User.Microservice.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var env = builder.Environment;
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors();
-builder.Services.AddControllers()
-    .AddJsonOptions(x => x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull);
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull);
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IJwtUtils, JwtUtils>();
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
@@ -33,24 +29,40 @@ builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(build
 //        ValidateAudience = false
 //    };
 //});
-//builder.Services.AddSwaggerGen(options =>
-//{
-//    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-//    {
-//        Description = "Standard authorization header using the bearer scheme",
-//        In = ParameterLocation.Header,
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.ApiKey
-//    });
-//    options.OperationFilter<SecurityRequirementsOperationFilter>();
-//    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Microservices Auth API", Version = "v1" });
-//});
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard authorization header using the bearer scheme",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Microservices Auth API", Version = "v1" });
+});
 
 
 var app = builder.Build();
 app.UseHttpsRedirection();
 
+
 //app.UseMiddleware<ExceptionMiddleware>();
+// add hardcoded test user to db on startup
+//using (var scope = app.Services.CreateScope())
+//{
+//    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+//    var testUser = new UserModel
+//    {
+//        FirstName = "Test",
+//        LastName = "User",
+//        Username = "test",
+//        PasswordHash = BCrypt.Net.BCrypt.HashPassword("test")
+//    };
+//    context.Users.Add(testUser);
+//    context.SaveChanges();
+//}
+
 app.UseMiddleware<ErrorHandlerMiddleware>();
 app.UseMiddleware<JwtMiddleware>();
 app.UseCors(x => x
