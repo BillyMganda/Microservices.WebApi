e1198fcdec641e98d96425dd77ed81515875390c
Billy <bimganda@gmail.com>
2023-05-08 02:25:51 -0400

    fix errors

--- User.Microservice/Authorization/JwtMiddleware.cs
+++ User.Microservice/Authorization/JwtMiddleware.cs
@@ -16,11 +16,12 @@ namespace User.Microservice.Authorization
         public async Task Invoke(HttpContext context, IUserService userService, IJwtUtils jwtUtils)
         {
             var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-            var userId = jwtUtils.ValidateJwtToken(token);
+            var userId = jwtUtils.ValidateJwtToken(token!);
+            var userIdGuid = Guid.Parse(userId!);
             if (userId != null)
             {
                 // attach user to context on successful jwt validation
-                context.Items["User"] = userService.GetById(userId.Value);
+                context.Items["User"] = userService.GetById(userIdGuid);
             }
 
             await _next(context);
