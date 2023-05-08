18d648bcdc0014a07a384416c627cd62f5fd3d6e
Billy <bimganda@gmail.com>
2023-05-08 05:38:49 -0400

    add dto class to add a new user

---
+++ User.Microservice/DTOs/AddUserRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.Microservice.DTOs
+{
+    public class AddUserRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required, EmailAddress]
+        public string Username { get; set; }
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+    }
+}
