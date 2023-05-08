c2d9907ad377fba9c2265c71f795222135dfcc84
Billy <bimganda@gmail.com>
2023-05-08 01:56:15 -0400

    add a db context class

---
+++ User.Microservice/Helpers/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using User.Microservice.Entities;
+
+namespace User.Microservice.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<UserModel> Users { get; set; }        
+    }
+}
