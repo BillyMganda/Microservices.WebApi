11cd5db07c8c3d61365031214d7db61fe77617e5
Billy <bimganda@gmail.com>
2023-05-08 02:01:55 -0400

    add dto class for auth request

---
+++ User.Microservice/DTOs/AuthenticateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.Microservice.DTOs
+{
+    public class AuthenticateRequest
+    {
+        [Required, EmailAddress]
+        public string Username { get; set; }
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+    }
+}
