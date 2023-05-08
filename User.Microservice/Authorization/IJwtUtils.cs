90687f818bfe8fb9b3bcdd15d334913805d90cbd
Billy <bimganda@gmail.com>
2023-05-08 02:23:55 -0400

    add jwt interface class with its methods and its implementation class

---
+++ User.Microservice/Authorization/IJwtUtils.cs
@@ -0,0 +1,11 @@
+﻿using User.Microservice.Entities;
+
+namespace User.Microservice.Authorization
+{
+    public interface IJwtUtils
+    {
+        public string GenerateJwtToken(UserModel user);
+        public string? ValidateJwtToken(string token);
+        public RefreshToken GenerateRefreshToken(string ipAddress);
+    }
+}
