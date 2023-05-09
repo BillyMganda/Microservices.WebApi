05247f4e3b9cb57aa72b3262be8db7e2cec4a26f
Billy <bimganda@gmail.com>
2023-05-09 06:06:12 -0400

    add mediatr and orders controller dependency to the test class

--- Test.Microservice/ordersControllerTest.cs
+++ Test.Microservice/ordersControllerTest.cs
@@ -1,6 +1,18 @@
-﻿namespace Test.Microservice
+﻿using Customer.Microservice.Controllers;
+using MediatR;
+using Moq;
+using Order.Microservice.Controllers;
+
+namespace Test.Microservice
 {
-    internal class ordersControllerTest
+    public class ordersControllerTest
     {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly ordersController _ordersController;
+        public ordersControllerTest()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _ordersController = new ordersController(_mediatorMock.Object);
+        }
     }
 }
